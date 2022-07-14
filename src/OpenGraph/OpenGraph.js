99a1f6f767e275248fa07e1c2d83464b6212c887
nftchance <chance@utc24.io>
2022-07-14 17:11:14 -0500

    adding default og image

--- src/OpenGraph/OpenGraph.js
+++ src/OpenGraph/OpenGraph.js
@@ -1,67 +1,56 @@
 import logo from "../images/logo.png"
+import hero from "../images/demo/colorspace.png";
 
 import "./OpenGraph.css";
 
 const OpenGraph = () => {
-    const queryParams = new URLSearchParams(
-        decodeURIComponent(window.location.search))
+    try {
+        const queryParams = new URLSearchParams(
+            decodeURIComponent(window.location.search))
 
-    let colors = null;
-    let gradient = null;
+        let colors = null;
+        let gradient = null;
+        let templateColumns = null
 
-    // Determine which color mode to use
-    if(queryParams.has('cs'))
-        colors = queryParams.getAll('cs')
-    else if(queryParams.has('g'))
-        gradient = queryParams.get('g')
+        // Determine which color mode to use
+        if (queryParams.has('cs')) {
+            colors = queryParams.getAll('cs')
 
-    const data = Array.from(queryParams.entries())
-    console.log('data', data)
+            // Determine how many columns are needed
+            templateColumns = colors
+                .map(() => "1fr")
+                .join(" ")
+        } else if (queryParams.has('g'))
+            gradient = queryParams.get('g')
 
-    // TODO: Gradient support needs to be implemented into the url generation
-    // Notes: this will be the most used state (unless they've chosen to show blocks to themselves in which case the share will be in that state)
-    // let gradient = null;
-    // if(!usingBlocks)
-    //     gradient = `
-    //         linear-gradient(
-    //             90deg,
-    //             hsl(0,0%,100%),
-    //             hsl(0,0%,80%),
-    //             hsl(0,0%,60%),
-    //             hsl(0,0%,40%),
-    //             hsl(0,0%,20%),
-    //             hsl(0,0%,0%)
-    //         )
-    //     `
+        // Catch the score 
+        const score = queryParams.get('s');
 
-    const score = 100;
+        return (
+            <div className="container">
+                <img src={logo} alt="logo" />
+                <h2>SCORED: {score} / 100</h2>
 
-    const templateColumns = colors
-        .map(() => "1fr")
-        .join(" ")
-
-    return (
-        <div className="container">
-            <img src={logo} alt="logo" />
-            <h2>SCORED: {score} / 100</h2>
-
-            <div 
-                className="step gradient"
-                style={{
-                    gridTemplateColumns: templateColumns,
-                    background: gradient
-                }}
-            >
-                {
-                    colors 
-                    ? colors.map(color => <div key={color} style={{ 
-                        background: color
-                    }}/>) 
-                    : null 
-                }
+                <div
+                    className="step gradient"
+                    style={{
+                        gridTemplateColumns: templateColumns,
+                        background: gradient
+                    }}
+                >
+                    {colors
+                        ? colors.map(color => <div key={color} style={{
+                            background: color
+                        }} />)
+                        : null
+                    }
+                </div>
             </div>
-        </div>
-    )
+        )
+    } catch {
+        // Return default OpenGraph image when dynamic is missing or fails
+        return <img src={hero} alt="hero" /> 
+    }
 }
 
 export default OpenGraph;
