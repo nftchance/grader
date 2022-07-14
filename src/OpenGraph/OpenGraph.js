bc8402e5b7441532bceaaab4571e3559491c0dd6
nftchance <chance@utc24.io>
2022-07-14 15:46:22 -0500

    adding base thumbnail

--- src/OpenGraph/OpenGraph.js
+++ src/OpenGraph/OpenGraph.js
@@ -1,8 +1,51 @@
 import "./OpenGraph.css";
 
-const OpenGraph = () => { 
+const OpenGraph = () => {
+    const colors = [
+        "#00ff00",
+        "#ff0000",
+        "#ffff00",
+        "#ffffff",
+    ]
+
+    const templateColumns = colors
+        .map(() => "1fr")
+        .join(" ")
+
+    const usingBlocks = false;
+    
+    let gradient = null;
+    if(!usingBlocks)
+        gradient = `
+            linear-gradient(
+                90deg,
+                hsl(0,0%,100%),
+                hsl(0,0%,80%),
+                hsl(0,0%,60%),
+                hsl(0,0%,40%),
+                hsl(0,0%,20%),
+                hsl(0,0%,0%)
+            )
+        `
+
     return (
-        <h1>Test</h1>
+        <div className="container">
+            <div 
+                className="step gradient"
+                style={{
+                    gridTemplateColumns: templateColumns,
+                    background: gradient
+                }}
+            >
+                {
+                    usingBlocks 
+                    ? colors.map(color => <div style={{ 
+                        background: color
+                    }}/>) 
+                    : null 
+                }
+            </div>
+        </div>
     )
 }
 
