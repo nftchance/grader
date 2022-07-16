7a8da69fc793584dc0f339ec94c052c856addcf5
nftchance <chance@utc24.io>
2022-07-15 20:59:08 -0500

    breaking apart math from rendering

---
+++ src/Three/ColorMath.js
@@ -0,0 +1,76 @@
+import * as THREE from 'three';
+
+export default class ColorMath {
+    colorMode = null;
+    SIZE = null;
+    SEGMENTS = null;
+
+    constructor(colorMode, SIZE, SEGMENTS) {
+        this.colorMode = colorMode;
+        this.SIZE = SIZE;
+        this.SEGMENTS = SEGMENTS;
+    }
+
+    // HANDLING THE CONVERSION OF POINTS TO ANGLES
+    p = (x, y) => {
+        return { x, y }
+    }
+
+    normalizeAngle = (angle) => {
+        if (angle < 0)
+            angle += (2 * Math.PI)
+
+        return angle
+    }
+
+    angle = (p1, center, p2) => {
+        const transformedP1 = this.p(p1.x - center.x, p1.y - center.y)
+        const transformedP2 = this.p(p2.x - center.x, p2.y - center.y)
+
+        const angleToP1 = Math.atan2(transformedP1.y, transformedP1.x)
+        const angleToP2 = Math.atan2(transformedP2.y, transformedP2.x)
+
+        return this.normalizeAngle(angleToP2 - angleToP1)
+    }
+
+    toDegrees = (radians) => {
+        return 360 * radians / (2 * Math.PI)
+    }
+
+    posToColor = (colorMode, x, y, z) => {
+        var color = new THREE.Color(0xffffff);
+
+        // CONVERT POSITION TO COLOR
+        if (colorMode === "RGB") {
+            const r = 0.5 + x / this.SIZE;       // R coordinate
+            const g = 0.5 + y / this.SIZE;       // G coordinate
+            const b = 0.5 + z / this.SIZE;       // B coordinate
+
+            color.setRGB(r, g, b);
+        } else if (colorMode === "HSL") {
+            // CALCULATE ANCLE AROUND CONE
+            let h = this.toDegrees(this.angle(
+                this.p(5, 0), 
+                this.p(0, 0), 
+                this.p(x, z)
+            )) / 360;
+
+            let s = 1;
+            let v = 0.5;
+
+            // DETERMINE IF THE POINT IS IN THE CENTER
+            if ((x, Math.abs(y), z) === (0, this.SIZE / 2, 0)) {
+                s = 0;              // saturation levels of zero
+                v = 0.5 - y / this.SIZE  // set the light levels for the center
+            }
+
+            color.setHSL(h, s, v)
+        }
+
+        return color
+    }
+
+    colorToPos = () => {
+
+    }
+}
--- src/Three/ColorVisualization.js
+++ src/Three/ColorVisualization.js
@@ -2,72 +2,15 @@ import { useLayoutEffect, useRef } from 'react';
 
 import * as THREE from 'three';
 
+import ColorMath from './ColorMath';
+
 export default function ColorVisualization(props) {
     const { mode, size: SIZE, segments: SEGMENTS } = props;
 
     const ref = useRef(null);
 
     useLayoutEffect(() => {
-        // HANDLING THE CONVERSION OF POINTS TO ANGLES
-        const p = (x, y) => {
-            return { x, y }
-        }
-
-        const normalizeAngle = (angle) => {
-            if (angle < 0)
-                angle += (2 * Math.PI)
-
-            return angle
-        }
-
-        const angle = (p1, center, p2) => {
-            const transformedP1 = p(p1.x - center.x, p1.y - center.y)
-            const transformedP2 = p(p2.x - center.x, p2.y - center.y)
-
-            const angleToP1 = Math.atan2(transformedP1.y, transformedP1.x)
-            const angleToP2 = Math.atan2(transformedP2.y, transformedP2.x)
-
-            return normalizeAngle(angleToP2 - angleToP1)
-        }
-
-        const toDegrees = (radians) => {
-            return 360 * radians / (2 * Math.PI)
-        }
-
-        const posToColor = (colorMode, x, y, z) => {
-            var color = new THREE.Color(0xffffff);
-
-            // CONVERT POSITION TO COLOR
-            if (colorMode === "RGB") {
-                const r = 0.5 + x / SIZE;       // R coordinate
-                const g = 0.5 + y / SIZE;       // G coordinate
-                const b = 0.5 + z / SIZE;       // B coordinate
-
-                color.setRGB(r, g, b);
-            } else if (colorMode === "HSL") {
-                // CALCULATE ANCLE AROUND CONE
-                let h = toDegrees(angle(p(5, 0), p(0, 0), p(x, z))) / 360;
-                let s = 1;
-                let v = 0.5;
-
-                // DETERMINE IF THE POINT IS IN THE CENTER
-                if ((x, Math.abs(y), z) === (0, SIZE / 2, 0)) {
-                    s = 0;              // saturation levels of zero
-                    v = 0.5 - y / SIZE  // set the light levels for the center
-                }
-
-                color.setHSL(h, s, v)
-            }
-
-            return color
-        }
-
-        const appendColorVertex = (colors, x, y, z) => {
-            const posColor = posToColor(mode, x, y, z)
-
-            // BUILD THE VERTEX COLOR ARRAY 3 VALUES AT A TIME
-            colors.push(posColor.r, posColor.g, posColor.b);
-        }
+        const colorMath = new ColorMath(mode, SIZE, SEGMENTS)
 
         // ENABLE THE ABILITY TO COLOR THE CUBE BY VERTEX
         const material = new THREE.MeshBasicMaterial({
@@ -106,7 +49,10 @@ export default function ColorVisualization(props) {
             const z = vertexPositions[vertex + 2];  // Z coordinate
 
             // DETERMINE THE PROPER COLOR FOR THIS VERTEX
-            appendColorVertex(colors, x, y, z);
+            const posColor = colorMath.posToColor(mode, x, y, z)
+
+            // BUILD THE VERTEX COLOR ARRAY 3 VALUES AT A TIME
+            colors.push(posColor.r, posColor.g, posColor.b);
         }
 
         // UPDATE THE GEOMETRY
