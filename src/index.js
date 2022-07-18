a2263c9ed9179cb5672a86dc13af0fc5823cd02c
nftchance <chance@utc24.io>
2022-07-18 15:09:29 -0500

    import cleaning

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import { BrowserRouter as Router } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
 
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 
 import App from './App';
-import { HelmetProvider } from 'react-helmet-async';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
