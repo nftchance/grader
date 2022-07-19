31c6e93aded365b0f19255e0757a94bac2356d59
nftchance <chance@utc24.io>
2022-07-19 17:12:06 -0500

    cleaning copy of tutorial

--- src/Tool/ToolIntro.js
+++ src/Tool/ToolIntro.js
@@ -8,24 +8,27 @@ const ToolIntro = ({ introEnabled, handleIntroExit }) => {
     const steps = [
         {
             element: '.cube',
-            intro: <p>Explore the 3D visualization of your generated color palette as each point is plotted within the colorspace.<br /><br/>(The more straight the line, the better.)</p>,
+            intro: <p>Explore the 3D visualization of your color palette as each point is plotted within the colorspace.<br /><br/>(The straighter the line, the higher the score.)</p>,
             position: 'right',
         }, {
             element: '.input',
-            intro: 'Customize the input and fine-tune all aspects to get it just right.',
+            intro: 'Customize the input and fine-tune all aspects of your palette to get it just right.',
             position: 'left',
         }, {
             element: '.code',
-            intro: '... and here is the web-friendly code of your color scale so that you can take it home with you.',
+            intro: 'Once you have found your palette you can find the copy-paste ready code.',
             position: 'left',
         }, {
             element: '.color-mode',
-            intro: 'Choose from any of the available output modes and fine-tune your scale to perfection.',
+            intro: 'Choose from any of the available color output modes and fine-tune your scale to perfection.',
             position: 'top',
         }, {
             element: '.scale-gradient',
             intro: 'Finally we have the 2D view of your scale in gradient form along with sliders to move a color to a different position.',
             position: 'top',
+        }, { 
+            element: '.tool',
+            intro: <p>That's it! Now get out there and create the palette of your dreams.</p>
         }
     ];
 
