62c413ded892e3ac40e12ab959719028526c6e6f
nftchance <chance@utc24.io>
2022-07-26 10:25:06 -0500

    fixing color picker

--- src/components/Form/ColorspaceColors.js
+++ src/components/Form/ColorspaceColors.js
@@ -1,26 +1,53 @@
+import { Button, Tooltip } from "@mui/material";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-import "./ColorspaceColor.css";
-import ColorspaceColor from "./ColorspaceColor";
+import ColorspaceTextField from "@components/Form/ColorspaceTextField";
+import ColorspaceColorpickerBox from "@components/Form/ColorspaceColorpickerBox";
 
-export default function ColorspaceColors(props) {
-    const {
-        colors,
-        handleColorChange,
-        handleColorLock,
-        handleColorRemove
-    } = props;
+import "./ColorspaceColors.css";
 
+const colorStyle = {
+    display: "grid",
+    gridTemplateColumns: "8fr 2fr 2fr",
+    alignItems: "center",
+    borderLeft: "none"
+}
+
+export default function ColorspaceColors({
+    colors,
+    handleColorChange,
+    handleColorLock
+}) {
     return (
         <div className="color-container">
             {colors.map((color, idx) => (
-                <ColorspaceColor 
-                    key={`${color.color}-${idx}`}
-                    color={color}
-                    idx={idx}
-                    handleColorChange={handleColorChange}
-                    handleColorLock={handleColorLock}
-                    handleColorRemove={handleColorRemove}
-                />
+                <div key={`color:${idx}`} style={colorStyle}>
+                    <ColorspaceTextField
+                        colorspaceColor={color}
+                        onChange={handleColorChange}
+                        label={`Color #${idx}`}
+                        className="color"
+                    />
+
+                    <div className="color-input-picker">
+                        <ColorspaceColorpickerBox
+                            color={color}
+                            handleColorChange={handleColorChange}
+                            idx={idx}
+                        />
+                    </div>
+
+                    <div style={{ marginLeft: 10 }}>
+                        <Tooltip title="Lock">
+                            <Button onClick={() => { handleColorLock(idx) }}>
+                                {color.locked
+                                    ? <FontAwesomeIcon icon={['fal', 'lock']} />
+                                    : <FontAwesomeIcon icon={['fal', 'lock-open']} />
+                                }
+                            </Button>
+                        </Tooltip>
+                    </div>
+                </div>
             ))}
         </div>
     )
